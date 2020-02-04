d6e1285178fd4ef28ed53a0ab3e441422e7584c9
Miley Wright <dishaliekwright@yahoo.com>
2020-02-03 22:54:17 -0500

    displays all orders before a date

--- queries.sql
+++ queries.sql
@@ -7,6 +7,11 @@ Join  Category
 on product.Categoryid = category.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT Id, CompanyName, ShipVia, o.OrderDate
+FROM [order] as o
+JOIN Shipper
+ON  o.ShipVia = Shipper.Id
+WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
